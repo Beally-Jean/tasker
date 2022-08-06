253f57a443dbff478c78024e23629e559b46c8d7
argento <olegbrovtsyn@mail.ru>
2022-08-06 13:55:48 +0300

    add templates of routes, controllers, repositories and .env.example

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const Router = require('express')
 const router = new Router()
 
+const userRouter = require('./userRouter')
+const taskRouter = require('./taskRouter')
+const workspaceRouter = require('./workspaceRouter')
+const getWorkspaces = require('./getWorkspacesRouter')
+
+router.use('/user', userRouter)
+router.use('/task', taskRouter)
+router.use('/workspace', workspaceRouter)
+router.use('/get_workspaces', getWorkspaces)
+
 module.exports = router
